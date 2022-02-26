be750e61f4d988c3169ea64d768f328d919df33a
Jerick Arcenal Navales <100425018+JerickNavales@users.noreply.github.com>
2022-02-26 14:03:31 +0800

    Add files via upload

---
+++ Removingwhite.java
@@ -0,0 +1,20 @@
+package Program;
+
+public class Removingwhite {
+
+  public static void main(String[] args) {
+  
+  
+          
+        String str1="Remove white spaces";  
+          
+        //Removes the white spaces using regex  
+        str1 = str1.replaceAll("\\s+", "");  
+          
+        System.out.println("String after removing all the white spaces : " + str1);  
+    }  
+
+
+  }
+
+
